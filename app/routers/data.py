7f93381151d464d6539b9c8016ec96a150f7defa
Seph29 <epsylone_29@hotmail.com>
2024-09-08 01:56:59 +0200

    Update data.py
    - add api key

--- app/routers/data.py
+++ app/routers/data.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Query
 from ..services.file_service import collect_data
+from ..config import settings
 
 router = APIRouter()
 
 @router.get("/store")
-def get_data():
+def get_data(api_key: str = Query(None)):
+    if api_key is None:
+        raise HTTPException(status_code=400, detail="API Key is required")
+    if api_key != settings.API_KEY:
+        raise HTTPException(status_code=403, detail="Invalid API Key")
     data = collect_data()
     return data
