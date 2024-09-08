50ccbaba1900ea21fd1f6d1ee8ada757d793e4d6
Seph29 <epsylone_29@hotmail.com>
2024-09-08 16:45:11 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -10,10 +10,10 @@ class Settings():
     SHARED_DIRECTORY_USERNAME: str = os.getenv("SHARED_DIRECTORY_USERNAME", "telmi")
     SHARED_DIRECTORY_PASSWORD: str = os.getenv("SHARED_DIRECTORY_PASSWORD", "telmipassword")
     TARGET_URL: str
-    BANNER_IMAGE: str = os.getenv("BANNER_IMAGE", "https://raw.githubusercontent.com/histoires-pour-tous/.github/main/profile/banner-telmi.jpg")
+    BANNER_IMAGE: str = os.getenv("BANNER_IMAGE", "https://raw.githubusercontent.com/Seph29/Telmi-store/main/custom-store.png")
     BANNER_LINK: str = os.getenv("BANNER_LINK", "https://monurl.ca/lunii.creations/")
-    BANNER_BACKGROUND: str = os.getenv("BANNER_BACKGROUND", "#016673")
-    LIBRARY_LINK: str = os.getenv("LIBRARY_LINK", "http://link.to.library/telmilib")
+    BANNER_BACKGROUND: str = os.getenv("BANNER_BACKGROUND", "#000000")
+    LIBRARY_LINK: str = os.getenv("LIBRARY_LINK", "http://#")
     API_KEY: str = os.getenv("API_KEY", None)
     if API_KEY is None:
         API_KEY = secrets.token_urlsafe(32)
