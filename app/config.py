cc44065cab964d121f2adc8f1344d47d2490f971
Seph29 <epsylone_29@hotmail.com>
2024-09-08 16:06:38 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
 import os
+import logging
+import secrets
 
 load_dotenv()
+
 class Settings():
     SHARED_DIRECTORY_PATH: str = os.getenv("SHARED_DIRECTORY_PATH", "/mnt/nfs/telmiLibrary")
     SHARED_DIRECTORY_USERNAME: str = os.getenv("SHARED_DIRECTORY_USERNAME", "telmi")
@@ -11,6 +14,9 @@ class Settings():
     BANNER_LINK: str = os.getenv("BANNER_LINK", "https://monurl.ca/lunii.creations/")
     BANNER_BACKGROUND: str = os.getenv("BANNER_BACKGROUND", "#016673")
     LIBRARY_LINK: str = os.getenv("LIBRARY_LINK", "http://link.to.library/telmilib")
-    API_KEY: str = os.getenv("API_KEY", "defaultapikey")
+    API_KEY: str = os.getenv("API_KEY", None)
+    if API_KEY is None:
+        API_KEY = secrets.token_urlsafe(32)
+
 
 settings = Settings()
