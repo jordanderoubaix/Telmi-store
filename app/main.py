00671b721b2646e78a8ab544fafaa39c78438cc4
Seph29 <epsylone_29@hotmail.com>
2024-09-08 16:06:14 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
+import logging
 from fastapi import FastAPI
 from routers import data
+from config import settings
 
 app = FastAPI()
 
@@ -8,3 +10,8 @@ app.include_router(data.router)
 @app.get("/")
 def read_root():
     return {"message": "Your telmi store API is running. Go to /store to get the data."}
+
+
+@app.on_event("startup")
+async def on_startup():
+    logging.info(f"A new API Key has been generated: {settings.API_KEY}")
