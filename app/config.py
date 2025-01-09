f1a1388f405791ad9f3475f515e0842a8a303a6d
Franck Alary <franck@alary.pro>
2025-01-09 14:05:34 +0100

    change default env values

--- app/config.py
+++ app/config.py
@@ -10,8 +10,8 @@ class Settings():
     SHARED_DIRECTORY_USERNAME: str = os.getenv("SHARED_DIRECTORY_USERNAME", "telmi")
     SHARED_DIRECTORY_PASSWORD: str = os.getenv("SHARED_DIRECTORY_PASSWORD", "telmipassword")
     TARGET_URL: str
-    BANNER_IMAGE: str = os.getenv("BANNER_IMAGE", "https://raw.githubusercontent.com/Seph29/Telmi-store/main/custom-store.png")
-    BANNER_LINK: str = os.getenv("BANNER_LINK", "https://monurl.ca/lunii.creations/")
+    BANNER_IMAGE: str = os.getenv("BANNER_IMAGE", "https://raw.githubusercontent.com/jordanderoubaix/Telmi-store/refs/heads/main/img/banner-custom-telmi-store.png")
+    BANNER_LINK: str = os.getenv("BANNER_LINK", "https://github.com/jordanderoubaix/Telmi-store")
     BANNER_BACKGROUND: str = os.getenv("BANNER_BACKGROUND", "#000000")
     LIBRARY_LINK: str = os.getenv("LIBRARY_LINK", "http://#")
     API_KEY: str = os.getenv("API_KEY", None)
